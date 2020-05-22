b621f5b9bb6d72c7bd4d4cba39b0dabaa21b24c9
may-tech <aehkloprsw@gmail.com>
2020-05-22 21:50:48 +0900

    [add] 1-5

---
+++ myapp.py
@@ -0,0 +1,55 @@
+# coding; utf-8
+"""
+Comment
+Comment
+Comment
+"""
+# Comment
+
+print("Hello World")
+
+#変数
+msg = "Hello World"
+print(msg)
+msg = "Hello Again"
+print(msg)
+ADMIN_EMAIL = "may@email.com"
+
+#文字列
+s = "he\nllo wor\tld"
+html = """<html>
+<body></body>
+</html>"""
+print(s)
+print(html)
+
+#整数
+i = 10
+
+#不動小数点
+f = 23.4
+
+#論理値
+flag = True # False
+
+
+#四則演算 = - * / // % **
+
+x = 10 
+print(x / 3)  #3.33...
+print(x // 3) #3
+print(x % 3)  #1
+print(x ** 3) #100
+
+y = 4
+y += 12       #y = y + 12
+print(y)      #16
+
+#and or not
+print(True and False) #False
+print(True or False)  #True
+print(not True)       #False
+
+#文字連結 + *
+print("hello " + "world")
+print("hello " * 3)
